38e383e568c9f0e1a23641910c0e69de52aa47b8
marcoazzurrini <marco.azzurrini.art@gmail.com>
2019-10-29 00:06:37 +0100

    add animation

--- js/app.js
+++ js/app.js
@@ -27,3 +27,30 @@ document.addEventListener('click', function(e) {
 
 // HEADER BRANDING ANIMATION (ANIMEJS)
 import anime from './anime.es.js';
+// const overlay = document.querySelector('.header-branding');
+const overlayMobile = document.querySelector('.header-branding-mobile');
+
+// var morphing = anime({
+//     targets: '.morph',
+//     d: [
+//         { value: 'M1920,0H868.4s366.482,115.282,497.641,497.641S1920,1043.2,1920,1043.2Z'},
+//         { value: 'M1920,0H868.4s-59.175,497.641,497.641,497.641S1920,1043.2,1920,1043.2Z'}
+
+//     ],
+//     // easing: 'easeInOutQuint',
+//     duration: 2500,
+//     loop: true,
+//     autoplay: true
+// });
+
+var morphingMobile = anime({
+    targets: '.morph-mobile',
+    d: [
+        { value: 'M.46,0H374.513V403.463S310.179,91.9,172.322,189.324.46,0,.46,0Z'},
+        { value: 'M.46,0H374.513V403.463s-3.676-207.705-202.191-214.138S.46,0,.46,0Z'}
+    ],
+    // easing: 'easeInOutQuint',
+    duration: 2500,
+    loop: true,
+    autoplay: true
+});
